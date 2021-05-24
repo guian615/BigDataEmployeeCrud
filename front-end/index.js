a05019ef44c1893b7f04ec995efe81a65d7d02f8
guian615 <guianamancio@gmail.com>
2021-05-24 13:49:30 +0800

    almost done

--- front-end/index.js
+++ front-end/index.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
     getUsers();
 
+   
+
 
     $('.add').click(() => {
-        $('.actionBtn').text("Add Employee");
-        $('.actionBtn').removeClass("add");
-        $('.actionBtn').addClass("update");
+
         $fullname = $('.fullname').val();
         $email = $('.email').val();
         $work = $('.work').val();
-        $img= $('.image').val();
-      
+        $img = $('.image').val();
+
         fetch('http://localhost:7000/user/add', {
             method: 'POST',
             body: JSON.stringify({
@@ -38,80 +38,76 @@ $(document).ready(function () {
                 data.forEach(element => {
                     $('.data').append(`
                                 <div class="col-lg-3 col-md-4 col-sm-12">
-                                <div class="card product_item">
-                                    <div class="card product_item">
+                               
+                                    <div class="card product_item" style="box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;">
                                         <div class="body">
                                             <div class="cp_img">
-                                                <img src="${element.imgUrl}" alt="Product" class="img-fluid">
+                                                <img src="${element.imgUrl}" alt="Product" style="height:200px;width:225px;">
                                                 <div class="hover">
-                                                <button type="button" onClick="editStudent('${element._id}')" class="btn btn-primary btn-sm waves-effect editBtn"data-toggle="modal" data-target="#exampleModal">Edit</button>
-                                                <button onClick="deleteStudent('${element._id}')" class="btn btn-danger btn-sm waves-effect deletes" >Delete</button>
+                                                <button type="button" onClick="editStudent('${element._id}')" class="btn btn-primary btn-sm waves-effect editBtn"data-toggle="modal" data-target="#updateModal"><i class="fa fa-pencil-square-o" style="font-size:15px"></i>
+                                                </button>
+                                                <button onClick="deleteStudent('${element._id}')" class="btn btn-danger btn-sm waves-effect deletes" ><i class="fa fa-trash" style="font-size:15px"></i> </button>
                                                 </div>
                                             </div>
                                             <div class="product_details">
-                                                <h5>${element.fullname}</h5>
-                                                <p>${element.position}</p>
-                                                <p>${element.email}</p>
+                                                <h5 class="text-center">${element.fullname}</h5>
+                                                <p class="mt-2"><b>Position:</b>  ${element.position}</p>
+                                                <p style="margin-top:-18px;"><b>Email:</b>  ${element.email}</p>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
+                               
                             </div>`)
 
                 });
-               
+
             }).catch(error => {
                 console.log(error);
             });
     }
 
     window.deleteStudent = function (id) {
-            if (confirm('Do you really want to delete this data?')) {
-                var parent = $(this).parent().parent();
-                $.ajax({
-                    type: "DELETE",
-                    url: "http://localhost:7000/user/" + id,
-                    cache: false,
-                    success: function() {
-                       
-                        location.reload(true)
-                    },
-                    error: function() {
-                        alert('Error deleting record');
-                    }
-                });
-            }
+        if (confirm('Do you really want to delete this data?')) {
+            var parent = $(this).parent().parent();
+            $.ajax({
+                type: "DELETE",
+                url: "http://localhost:7000/user/" + id,
+                cache: false,
+                success: function () {
+
+                    location.reload(true)
+                },
+                error: function () {
+                    alert('Error deleting record');
+                }
+            });
+        }
     }
 
 
 
     window.editStudent = function (id) {
-
-      $('.actionBtn').text("Update");
-      $('.actionBtn').removeClass("add");
-      $('.actionBtn').addClass("update");
-
         fetch(`http://localhost:7000/user/${id}`)
             .then(res => res.json())
             .then((data) => {
-                $('.fullname').val(data.fullname);
-                $('.work').val(data.position);
-                $('.email').val(data.email);
-                $('.image').val(data.imgUrl);
-                $('.userid').val(data._id);
+                $('.updatefullname').val(data.fullname);
+                $('.updatework').val(data.position);
+                $('.updateemail').val(data.email);
+                $('.updateimage').val(data.imgUrl);
+                $('.updateuserid').val(data._id);
             });
 
     }
 
 
     $('.update').click(() => {
-        $fullname = $('.fullname').val();
-        $email = $('.email').val();
-        $work = $('.work').val();
-        $image = $('.image').val();
-        $id = $('.userid').val()
+        $fullname = $('.updatefullname').val();
+        $email = $('.updateemail').val();
+        $work = $('.updatework').val();
+        $image = $('.updateimage').val();
+        $id = $('.updateuserid').val()
         console.log($id)
-        fetch('http://localhost:7000/user/update/'+$id, {
+        fetch('http://localhost:7000/user/update/' + $id, {
             method: 'POST',
             body: JSON.stringify({
                 fullname: $fullname,
@@ -120,10 +116,10 @@ $(document).ready(function () {
                 imgUrl: $image,
             }),
             headers: {
-                'Content-Type' : 'application/json'
+                'Content-Type': 'application/json'
             }
         })
         location.reload(true)
     })
-   
+
 })
